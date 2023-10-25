f912346f511daeff94698ab6796dba6d8d9bed2f
webdesignbytm <logoeje@gmail.com>
2023-10-25 17:02:18 +0300

    Lab 3

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -13,12 +13,21 @@
 
 typedef std::tuple <float, float, float> LineEq;
 typedef std::mt19937 RNGeng;
+struct LocalPeak 
+{
+    int theta, ro, hval;
+    bool operator < (LocalPeak const& other) const
+    {
+        return hval > other.hval;
+    }
+};
 RNGeng engine;
 std::string INPUT_PREFIX = "c:\\Users\\logoeje\\source\\repos\\PRS\\Inputs\\";
-std::string LAB_FOLDER = "lab2\\";
-std::string FILE_NAME = "points";
-int NO_TESTS = 6;
-//int NO_TESTS = 2;
+std::string LAB_FOLDER = "lab3\\";
+std::string FILE_NAME = "edge_";
+std::string TEST_NAME[] = {"simple", "complex"};
+//int NO_TESTS = 6;
+int NO_TESTS = 2;
 
 std::pair <float, float> calculateParameters(int noPoints, std::vector<Point2f> const &points)
 {
@@ -82,92 +91,98 @@ float calculateLineDist(LineEq const &line, Point p)
     return (abs(1LL*a * p.x + 1LL*b * p.y + c) / sqrt(1LL*a * a + 1LL*b * b));
 }
 
+void findLocalMax(Mat_<int> const &src, int i, int j, int l, std::vector<LocalPeak> &collector)
+{
+    int trueY, trueX;
+    //std::cout << "Trying for " << i << ' ' << j << " -> " << (int)src(i, j) << '\n';
+    for (int y = -l / 2; y <= l / 2; ++y)
+        for (int x = -l / 2; x <= l / 2; ++x)
+        {
+            trueY = (src.rows + i + y) % src.rows;
+            trueX = (src.cols + j + x) % src.cols;
+            if (trueY == i && trueX == j)
+                continue;
+            //std::cout << trueY << ' ' << trueX << " -> " << (int) src(trueY, trueX) << '\n';
+            if (src(i, j) < src(trueY, trueX))
+                return;
+        }
+
+    collector.push_back({ j, i, (int) src(i, j) });
+}
+
+float rad(const int degree) { return degree * (PI / 180.0); }
+
 int main()
 {
-    // Far too many variables
     std::string fpath;
-    std::vector<Point> points;
-    LineEq bestParams;
-    LineEq crtParams;
     Mat_<uchar> img;
-    std::set<std::tuple<int, int, int, int> > usedSamples;
-    Point p1, p2;
-    float distThreshold = 10; // t
-    float targetCertainty = 0.99; // p
-    float expectedInliers = 0.8; // q
-    int sampleSize = 2; // s
-    int totalTrials; // N
-    int consensusSize; // T
-    int bestConsensus;
-    int crtConsensus;
-    uint32_t seed = std::time(nullptr);
-    std::uniform_int_distribution<uint32_t> uDist;
-    engine.seed(seed);
+    Mat_<Vec3b> displayImg;
+    Mat_<int> H;
+    Mat_<uchar> displayH;
+    std::vector<LocalPeak> localMax;
+    float ro;
+    float diagonal;
+    float maxFreq;
+    float resolutionEPS = 0.1f;
 
-    for (int testNo = 1; testNo < NO_TESTS; ++testNo)
+    for (int testNo = 0; testNo < NO_TESTS; ++testNo)
     {
         // Initialize
-        fpath = INPUT_PREFIX + LAB_FOLDER + FILE_NAME + std::to_string(testNo) + ".bmp";
+        fpath = INPUT_PREFIX + LAB_FOLDER + FILE_NAME + TEST_NAME[testNo] + ".bmp";
         img = imread(fpath, CV_LOAD_IMAGE_GRAYSCALE);
-        usedSamples.clear();
-        points.clear();
-        bestConsensus = crtConsensus = 0;
-        bestParams = bestParams = { 0, 0, 0 };
-        expectedInliers = (testNo == 1) ? 0.3 : 0.8;
+        diagonal = sqrt(img.rows * img.rows + img.cols * img.cols);
+        H = Mat_<int>((int) diagonal, 360);
+        displayH = Mat_<uchar>((int) diagonal, 360);
+        H.setTo(0);
+        displayH.setTo(0);
+        maxFreq = 0;
+        localMax.clear();
 
         for (int i = 0; i < img.rows; ++i)
             for (int j = 0; j < img.cols; ++j)
-                if (!img(i, j))
-                    points.push_back({ j, i });
+                if (img(i, j))
+                    for (float theta = 0; theta < 360; ++theta)
+                    {
+                        ro = j * cos(theta * PI / 180.0f) + i * sin(theta * PI / 180.0f);
+                        if (0 < ro && ro < H.rows)
+                            maxFreq = max(maxFreq, ++H((int)ro, (int)theta));
+                    }
 
-        // Calculate metaparameters
-        uDist = std::uniform_int_distribution<uint32_t>(0, points.size() - 1);
-        totalTrials = (int)(log(1 - targetCertainty) / log(1 - pow(expectedInliers, sampleSize)));
-        consensusSize = (int)(expectedInliers * points.size());
+        for (int i = 0; i < H.rows; ++i)
+            for (int j = 0; j < H.cols; ++j)
+                displayH(i, j) = (255.0f * H(i, j)) / maxFreq;
 
-        // Test samples
-        while (totalTrials-- && bestConsensus < consensusSize)
-        {
-            // Select sample -- hardcoded for 2 elements, cannot determine the line of more than 2 points unless they all are colinear
-            do {
-                p1 = points.at(uDist(engine));
-                p2 = points.at(uDist(engine));
-            } while (usedSamples.count({p1.x, p1.y, p2.x, p2.y}) != 0 
-                || usedSamples.count({ p2.x, p2.y, p1.x, p1.y }) != 0
-                || p1 == p2);
-            usedSamples.insert({ p1.x, p1.y, p2.x, p2.y });
+        //imshow("Frequencies", displayH);
+        //waitKey(0);
 
-            crtConsensus = 0;
-            crtParams = calculateLineEq(p1, p2);
-            for (auto p : points)
-                crtConsensus += (calculateLineDist(crtParams, p) <= distThreshold);
-            //std::cout << p1 << ' ' << p2 << " -> " << crtConsensus << '\n';
-
-            if (crtConsensus > bestConsensus)
+        for (int k = 3; k < 15; k += 4) {
+            localMax.clear();
+            for (int i = 0; i < displayH.rows; ++i)
+                for (int j = 0; j < displayH.cols; ++j)
+                {
+                    findLocalMax(H, i, j, k, localMax);
+                }
+            displayImg = Mat_<Vec3b>(img.rows, img.cols);
+            for (int i = 0; i < displayImg.rows; ++i)
+                for (int j = 0; j < displayImg.cols; ++j)
+                    displayImg(i, j) = { img(i, j), img(i, j), img(i, j) };
+            std::sort(localMax.begin(), localMax.end());
+            for (int lines = 0; lines < min(10, localMax.size()); ++lines)
             {
-                bestConsensus = crtConsensus;
-                bestParams = crtParams;
+
+                Point pt1, pt2;
+                double a = cos(rad(localMax.at(lines).theta)), b = sin(rad(localMax.at(lines).theta));
+                double x0 = a * localMax.at(lines).ro, y0 = b * localMax.at(lines).ro;
+                pt1.x = cvRound(x0 + 1000 * (-b));
+                pt1.y = cvRound(y0 + 1000 * (a));
+                pt2.x = cvRound(x0 - 1000 * (-b));
+                pt2.y = cvRound(y0 - 1000 * (a));
+                line(displayImg, pt1, pt2, { 0, 0, 255 });
             }
+            imshow("Result", displayImg);
+            waitKey(0);
         }
 
-        double a, b, c;
-        std::tie(a, b, c) = bestParams;
-        if (abs(b) > 10)
-        {
-            p1 = { 0,  (int)(-c / b) };
-            p2 = { img.cols - 1, (int)-(1LL*((img.cols - 1) * a + c) / b) };
-        }
-        else
-        {
-            p1 = { (int)(-c / a), 0};
-            p2 = { (int)-((1LL*(img.rows - 1) * b + c) / a), img.rows - 1 };
-        }
-        line(img, p1, p2, {0, 0, 0});
-        //std::cout << "Line equation: " << a << ' ' << b << ' ' << c << '\n';
-        //std::cout << "Finally: " << p1 << ' ' << p2 << '\n';
-
-        imshow("Result", img);
-        waitKey(0);
     }
 
 	return 0;
